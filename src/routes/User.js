71b4504083cad5e8e46365153828904e8e138827
Miracle Chukwuebuka C. O <57507288+miracleCA@users.noreply.github.com>
2024-08-07 23:01:40 +0100

    Update User.js

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verifyToken } from '../auth.js';
+import { verifyToken } from '../Auth.js';
 import mongoose from 'mongoose';
 import { UserModel } from "../models/User.js";
 
